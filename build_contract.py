68ef5ecda3c927ffc15f4678154b70ae22c4c814
Rachiken <z390124578@gmail.com>
2021-04-11 02:16:17 +0800

    new contract

--- build_contract.py
+++ build_contract.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
                     contract HyperParameter_LR {
 
                     uint min = 0;
-                    uint max = 299;
+                    uint max = 8250;
                     uint counter = 0;
-                    uint[300] acc;
+                    uint[8249] acc;
 
                     event parameter_log(uint counter);
 
