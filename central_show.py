d60b4f16a44adf41ed2ad034eb2cbfa959cf5d7e
rachiken <z390124578@gmail.com>
2021-06-13 05:40:43 +0800

    show data from specific dataset

--- central_show.py
+++ central_show.py
@@ -1,5 +1,11 @@
+import sys
+
 if __name__ == "__main__":
-    with open('central.txt', 'r') as f:
+    if len(sys.argv) > 1:
+        dataset = sys.argv[1]
+    else:
+        dataset = 'mnist'
+    with open(dataset + 'central.txt', 'r') as f:
         record = [ float(l.strip('\n'))/100 for l in f]
 
     best = max(record)
